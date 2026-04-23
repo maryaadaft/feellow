f8dfb9a47a6723f3e7c14513a812bb5de2041791
Maryam Adam <maryaada@lab2r3s14.42abudhabi.ae>
2026-04-23 15:49:06 +0400

    structure

---
+++ philo/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maryaada <maryaada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/23 13:12:49 by maryaada          #+#    #+#             */
+/*   Updated: 2026/04/23 13:15:19 by maryaada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <limits.h>
+
+#endif
