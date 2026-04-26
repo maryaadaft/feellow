290254dc76b1991eb1ce05e511e16f7fc194f910
maryaadaft <maryaada@student.42abudhabi.ae>
2026-04-26 12:11:12 +0400

    i forgot what i did

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -12,7 +12,21 @@
 
 #include "philosophers.h"
 
-int	main(void)
+void  parse_args(int argc, char **argv)
+{
+	if (argc < 5 || argc > 6)
+		exit(1); //why exit(1) and not exit(0)
+
+	if(argc == 5)
+		//validate numbers
+		//atoi(argv[1] -> argv[5]) 4 arguments
+	if(argc == 6)
+		//validate numbers and exit program at 5th argument
+		//with a flag maybe????
+}
+
+int	main(int argc, char **argv)
 {
 	//handle args
+	
 }
